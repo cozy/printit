9d68cf39f610b006f24b06d9e1cf7beea65e2104
Frank Rousseau <frank.rousseau@cozycloud.cc>
2013-12-18 12:19:17 +0100

    add date-format lib

---
+++ date-format.js
@@ -0,0 +1,109 @@
+/*
+* @version  0.5.0
+* @author   Lauri Rooden - https://github.com/litejs/date-format-lite
+* @license  MIT License  - http://lauri.rooden.ee/mit-license.txt
+*/
+
+
+!function(Date, proto) {
+	var maskRe = /(["'])((?:[^\\]|\\.)*?)\1|YYYY|([MD])\3\3(\3?)|SS|([YMDHhmsW])(\5?)|[uUAZSwo]/g
+	, yearFirstRe = /(\d{4})[-.\/](\d\d?)[-.\/](\d\d?)/
+	, dateFirstRe = /(\d\d?)[-.\/](\d\d?)[-.\/](\d{4})/
+	, timeRe = /(\d\d?):(\d\d):?(\d\d)?\.?(\d{3})?(?:\s*(?:(a)|(p))\.?m\.?)?(\s*(?:Z|GMT|UTC)?(?:([-+]\d\d):?(\d\d)?)?)?/i
+	, wordRe = /.[a-z]+/g
+	, unescapeRe = /\\(.)/g
+
+	// ISO 8601 specifies numeric representations of date and time.
+	//
+	// The international standard date notation is
+	// YYYY-MM-DD
+	//
+	// The international standard notation for the time of day is
+	// hh:mm:ss
+	//
+	// Time zone
+	//
+	// The strings +hh:mm, +hhmm, or +hh (ahead of UTC)
+	// -hh:mm, -hhmm, or -hh (time zones west of the zero meridian, which are behind UTC)
+	//
+	// 12:00Z = 13:00+01:00 = 0700-0500
+
+	Date[proto].format = function(mask) {
+		mask = Date.masks[mask] || mask || Date.masks["default"]
+
+		var self = this
+		, get = "get" + (mask.slice(0,4) == "UTC:" ? (mask=mask.slice(4), "UTC"):"")
+
+		return mask.replace(maskRe, function(match, quote, text, MD, MD4, single, pad) {
+			text = single == "Y"   ? self[get + "FullYear"]() % 100
+				 : match == "YYYY" ? self[get + "FullYear"]()
+				 : single == "M"   ? self[get + "Month"]()+1
+				 : MD     == "M" ? Date.monthNames[ self[get + "Month"]()+(MD4 ? 12 : 0) ]
+				 : single == "D"   ? self[get + "Date"]()
+				 : MD     == "D" ? Date.dayNames[ self[get + "Day"]() + (MD4 ? 7:0 ) ]
+				 : single == "H"   ? self[get + "Hours"]() % 12 || 12
+				 : single == "h"   ? self[get + "Hours"]()
+				 : single == "m"   ? self[get + "Minutes"]()
+				 : single == "s"   ? self[get + "Seconds"]()
+				 : match == "S"    ? self[get + "Milliseconds"]()
+				 : match == "SS"   ? (quote = self[get + "Milliseconds"](), quote > 99 ? quote : (quote > 9 ? "0" : "00" ) + quote)
+				 : match == "u"    ? (self/1000)>>>0
+				 : match == "U"    ? +self
+				 : match == "A"    ? Date[self[get + "Hours"]() > 11 ? "pm" : "am"]
+				 : match == "Z"    ? "GMT " + (-self.getTimezoneOffset()/60)
+				 : match == "w"    ? self[get + "Day"]() || 7
+				 : single == "W"   ? (quote = new Date(+self + ((4 - (self[get + "Day"]()||7)) * 86400000)), Math.ceil(((quote.getTime()-quote["s" + get.slice(1) + "Month"](0,1)) / 86400000 + 1 ) / 7) )
+				 : match == "o"    ? new Date(+self + ((4 - (self[get + "Day"]()||7)) * 86400000))[get + "FullYear"]()
+				 : quote           ? text.replace(unescapeRe, "$1")
+				 : match
+			return pad && text < 10 ? "0"+text : text
+		})
+	}
+
+	Date.am = "AM"
+	Date.pm = "PM"
+
+	Date.masks = {"default":"DDD MMM DD YYYY hh:mm:ss","isoUtcDateTime":'UTC:YYYY-MM-DD"T"hh:mm:ss"Z"'}
+	Date.monthNames = "JanFebMarAprMayJunJulAugSepOctNovDecJanuaryFebruaryMarchAprilMayJuneJulyAugustSeptemberOctoberNovemberDecember".match(wordRe)
+	Date.dayNames = "SunMonTueWedThuFriSatSundayMondayTuesdayWednesdayThursdayFridaySaturday".match(wordRe)
+
+	//*/
+
+
+	/*
+	* // In Chrome Date.parse("01.02.2001") is Jan
+	* n = +self || Date.parse(self) || ""+self;
+	*/
+
+	String[proto].date = Number[proto].date = function(format) {
+		var m, temp
+		, d = new Date
+		, n = +this || ""+this
+
+		if (isNaN(n)) {
+			// Big endian date, starting with the year, eg. 2011-01-31
+			if (m = n.match(yearFirstRe)) d.setFullYear(m[1], m[2]-1, m[3])
+
+			else if (m = n.match(dateFirstRe)) {
+				// Middle endian date, starting with the month, eg. 01/31/2011
+				// Little endian date, starting with the day, eg. 31.01.2011
+				temp = Date.middle_endian ? 1 : 2
+				d.setFullYear(m[3], m[temp]-1, m[3-temp])
+			}
+
+			// Time
+			m = n.match(timeRe) || [0, 0, 0]
+			d.setHours( m[6] && m[1] < 12 ? +m[1]+12 : m[5] && m[1] == 12 ? 0 : m[1], m[2], m[3]|0, m[4]|0)
+			// Timezone
+			if (m[7]) {
+				d.setTime(d-((d.getTimezoneOffset() + (m[8]|0)*60 + ((m[8]<0?-1:1)*(m[9]|0)))*60000))
+			}
+		} else d.setTime( n < 4294967296 ? n * 1000 : n )
+		return format ? d.format(format) : d
+	}
+
+}(Date, "prototype")
+
+
+
+
